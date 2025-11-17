17a9be1c4588c3f5fc5be6e360e09f5fabf8e82e
Moses Hunsu <hunsumoses7@gmail.com>
2025-11-17 21:12:01 +0100

    Added wallet repository

---
+++ src/main/java/com/hts/walletservice/repository/WalletRepository.java
@@ -0,0 +1,13 @@
+package com.hts.walletservice.repository;
+
+import com.hts.walletservice.model.Wallet;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface WalletRepository extends R2dbcRepository<Wallet, UUID> {
+
+    Mono<Wallet> findByUserId(String userId);
+
+}
